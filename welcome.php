59a6bb7f3395c28707fc667fb236fb97f6dfda84
shivank gouttam <shivankgouttam1999@gmail.com>
2023-09-17 23:25:47 +0530

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["Name"]; ?><br>
+Your email address is: <?php echo $_POST["Email"]; ?><br>
+Your phone number is: <?php echo $_POST["Phone"]; ?><br>
+and your message is <?php echo $_POST["Message"]; ?><br>
+
+  <br><br>
+  <h1>
+<a href="index.html">Back to the Home page</a>
+  </h1>
+</body>
+</html>
